b3a040c5f7a6a92f84a1dd140c37933cc796e1b9
Manoela Frattini <manoelamoreno78@gmail.com>
2023-01-26 13:58:11 -0300

    mobile

--- assets/js/modules/page.js
+++ assets/js/modules/page.js
@@ -91,9 +91,11 @@
 		autoplaySpeed: 4000,
 		pauseOnFocus: false,
 		pauseOnHover: false,
-		arrows: false,
+		arrows: true,
 		dots: false,
 		adaptiveHeight: true,
+		prevArrow: $('.slick-nav_historia').find('.slick-prev'),
+        nextArrow: $('.slick-nav_historia').find('.slick-next'),
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		responsive: [{
@@ -105,7 +107,10 @@
 						}
 			
 			}]
-	});
+	}).on('afterChange', function(event, slick, currentSlide, nextSlide){
+		var current = currentSlide + 1;
+		$('.slick-nav_historia').find('.slick-numbers .current').html(current < 10 ? "0" + current : current)}
+);
 
 	function copyToClipboard(element) {
 		var $temp = $("<input>");
